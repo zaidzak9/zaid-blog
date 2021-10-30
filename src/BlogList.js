025ece94a958d51e7b280c7a90db173e292f35f3
ZaidZakir <zaidzakir@hlbb.hongleong.com.my>
2021-10-30 11:16:13 +0800

    Added fake json to simulate api and chnages to CSS

---
+++ src/BlogList.js
@@ -0,0 +1,16 @@
+const BlogList = ({blogs,handleDelete}) => {
+
+    return ( 
+        <div className="blog-list">
+             {blogs.map(blog => (
+                <div className="blog-preview" key={blog.id} >
+                    <h2>{blog.title}</h2>
+                    <p>Written by {blog.author}</p>
+                    <button onClick = {() => handleDelete(blog.id)}>Delete Blog</button>
+                </div>
+            ))}
+        </div>
+     );
+}
+ 
+export default BlogList;
--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,19 @@
+import { useState } from "react";
+import BlogList from "./BlogList";
+
 const Home = () => {
-    return ( 
+    const [blogs, setBlogs] = useState(null)
+
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter(blog => blog.id !== id );
+        setBlogs(newBlogs);
+    }
+
+    return (
         <div className="home">
-            <h2>Home Page</h2>
+           <BlogList blogs = {blogs} handleDelete = {handleDelete}/>
         </div>
-     );
+    );
 }
- 
+
 export default Home;
